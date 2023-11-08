4e5a7b540ab9c093c8e65cae99dd616e52d8ede3
Christopher <christopher.cornet@laplateforme.io>
2023-11-08 10:50:32 +0100

    Add click events on arrow

---
+++ assets/script.js
@@ -0,0 +1,29 @@
+const slides = [
+	{
+		"image":"slide1.jpg",
+		"tagLine":"Impressions tous formats <span>en boutique et en ligne</span>"
+	},
+	{
+		"image":"slide2.jpg",
+		"tagLine":"Tirages haute définition grand format <span>pour vos bureaux et events</span>"
+	},
+	{
+		"image":"slide3.jpg",
+		"tagLine":"Grand choix de couleurs <span>de CMJN aux pantones</span>"
+	},
+	{
+		"image":"slide4.png",
+		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
+	}
+]
+
+let arrow_left = document.getElementById("arrow_left");
+let arrow_right = document.getElementById("arrow_right");
+
+arrow_left.addEventListener("click", function click() {
+	console.log("left");
+});
+
+arrow_right.addEventListener("click", function click() {
+	console.log("right");
+});
