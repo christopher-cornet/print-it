9a6dcbc7902c43de677610596f784bfcbedb5188
Christopher <christopher.cornet@laplateforme.io>
2023-11-10 14:53:03 +0100

    Dots position change works to the right

--- assets/script.js
+++ assets/script.js
@@ -17,13 +17,71 @@ const slides = [
 	}
 ]
 
-let arrow_left = document.getElementById("arrow_left");
-let arrow_right = document.getElementById("arrow_right");
+// Variables and elements
+const arrow_left = document.querySelector(".arrow_left");
+const arrow_right = document.querySelector(".arrow_right");
 
-arrow_left.addEventListener("click", function click() {
+let dots = document.querySelector(".dots");
+
+let index = 0; // Index in slides
+
+
+// queryselectorall pour avoir ts les bullet points
+
+
+// Create the bullet points
+let addBulletPoints = () => {
+	for (element in slides) {
+		let bulletPoint = document.createElement("div");
+		bulletPoint.classList.add("dot", `dot${element}`);
+		dots.appendChild(bulletPoint);
+	}
+	let bulletPoint1 = document.querySelector(".dot0");
+	bulletPoint1.classList.add("dot_selected");
+}
+
+// Change the selected bullet point
+let changeSlide = () => {
+	let bulletPoint0 = document.querySelector(".dot0");
+	let bulletPoint1 = document.querySelector(".dot1");
+	let bulletPoint2 = document.querySelector(".dot2");
+	let bulletPoint3 = document.querySelector(".dot3");
+
+	if (bulletPoint0.classList.contains("dot_selected")) {
+		bulletPoint0.classList.toggle("dot_selected");
+		bulletPoint1.classList.add("dot_selected");
+		index++;
+	}
+	else if (bulletPoint1.classList.contains("dot_selected")) {
+		bulletPoint1.classList.toggle("dot_selected");
+		bulletPoint2.classList.add("dot_selected");
+		index++;
+	}
+	else if (bulletPoint2.classList.contains("dot_selected")) {
+		bulletPoint2.classList.toggle("dot_selected");
+		bulletPoint3.classList.add("dot_selected");
+		index++;
+	}
+	else if (bulletPoint3.classList.contains("dot_selected")) {
+		bulletPoint3.classList.toggle("dot_selected");
+		bulletPoint0.classList.add("dot_selected");
+		index = 0;
+	}
+
+	console.log(index); // 0, 1, 2, 3 en boucle
+}
+
+addBulletPoints();
+
+// Events
+
+// Left arrow
+arrow_left.addEventListener("click", () => {
 	console.log("left");
 });
 
-arrow_right.addEventListener("click", function click() {
+// Right arrow
+arrow_right.addEventListener("click", () => {
 	console.log("right");
+	changeSlide();
 });
